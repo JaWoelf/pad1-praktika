881f3102f00f112c812e81b00a67154160b4d79c
jawo <j.woelfelschneider@zweihorn-design.de>
2021-05-18 17:19:07 +0200

    praktikum 3 aufgabe 2 fertig praktikum 3 aufgabe 3 fortschritt

--- praktikum_3_aufgabe_2/main.cpp
+++ praktikum_3_aufgabe_2/main.cpp
@@ -1,94 +1,92 @@
 #include <iostream>
-#include <vector>
-#include <bits/stdc++.h>
-
+#include <regex>
+#include <string>
 using namespace std;
 
+// Figures
+// WHITE FIGURES
+const string wB = " w_BAUER___ ";
+const string wT = " w_TURM____ ";
+const string wS = " w_SPRINGER ";
+const string wL = " w_LÄUFER__ ";
+const string wD = " w_DAME____ ";
+const string wK = " w_KOENIG__ ";
+// BLACK FIGURES
+const string bB = " s_BAUER___ ";
+const string bT = " s_TURM____ ";
+const string bS = " s_SPRINGER ";
+const string bL = " s_LÄUFER__ ";
+const string bD = " s_DAME____ ";
+const string bK = " s_KOENIG__ ";
+// FREE FIELD
+const string fF = " __________ ";
+
+// BOARD
+string board[8][8] = {
+  {bT,bS,bL,bK,bD,bL,bS,bT},
+  {bB,bB,bB,bB,bB,bB,bB,bB},
+  {fF,fF,fF,fF,fF,fF,fF,fF},
+  {fF,fF,fF,fF,fF,fF,fF,fF},
+  {fF,fF,fF,fF,fF,fF,fF,fF},
+  {fF,fF,fF,fF,fF,fF,fF,fF},
+  {wB,wB,wB,wB,wB,wB,wB,wB},
+  {wT,wS,wL,wK,wD,wL,wS,wT}
+};
+
+// User Input
+string input;
+regex pattern("[a-h][1-8]-[a-h][1-8]");
+int fromx, fromy, tox, toy;
+
+/*
+ *    RUNTIME
+ */
+
 int main(){
-  const string w_B = " w_BAUER___ "; // WHITE FIGURES
-  const string w_T = " w_TURM____ ";
-  const string w_S = " w_SPRINGER ";
-  const string w_L = " w_LÄUFER__ ";
-  const string w_D = " w_DAME____ ";
-  const string w_K = " w_KOENIG__ ";
-  const string b_B = " s_BAUER___ "; // BLACK FIGURES
-  const string b_T = " s_TURM____ ";
-  const string b_S = " s_SPRINGER ";
-  const string b_L = " s_LÄUFER__ ";
-  const string b_D = " s_DAME____ ";
-  const string b_K = " s_KOENIG__ ";
-  const string f_F = " __________ "; // FREE FIELD
 
-  /*
-   * VARs
-   * ------------------------------------------
-   */
-  // Board with start setup.
-  string board[8][8] = {
-    {b_T,b_S,b_L,b_K,b_D,b_L,b_S,b_T},
-    {b_B,b_B,b_B,b_B,b_B,b_B,b_B,b_B},
-    {f_F,f_F,f_F,f_F,f_F,f_F,f_F,f_F},
-    {f_F,f_F,f_F,f_F,f_F,f_F,f_F,f_F},
-    {f_F,f_F,f_F,f_F,f_F,f_F,f_F,f_F},
-    {f_F,f_F,f_F,f_F,f_F,f_F,f_F,f_F},
-    {w_B,w_B,w_B,w_B,w_B,w_B,w_B,w_B},
-    {w_T,w_S,w_L,w_K,w_D,w_L,w_S,w_T}
-  };
-  // Last User Input
-  char userInputArray[5];
-
-  // Move
-  char sourceHorizontal,
-      sourceVertical,
-      targetHorizontal,
-      targetVertical;
-
-  while (true){
-    /*
-    *   DRAW BOARD
-    * ------------------------------------------
-    */
-
-    // Clear CLI
+  // Play-Time
+  while (true)
+  {
+    // Clear CLI and draw horizontal index
     cout << "\x1B[2J\x1B[H";
-    // draw horizontal Index
     cout << "\n         A           B           C           D           E           F           G           H       ";
 
-    for (int i = 0; i < 8; i++){
-        // draw vertical Index
-        cout << "\n  " << i+1 << " ";
-        for (int j = 0; j < 8; j++){
-            // Place Figures
-          cout << board[i][j];
-        } // vertical
-    } // horizontal
+    // draw board (with figures) line by line
+    // beginning each line with vertical index
+    for (int y = 0; y < 8; y++)
+    {
+      cout << "\n " << y+1 << " ";
+      for (int x = 0; x < 8; x++)
+      {
+        cout << board[y][x];
+      }
+    }
+
+    // User Input Area
+    cout << "\n\n  Geben Sie den nächsten Zug ein.\nz.B. d7-a4 'exit' beendet das Spiel.\n";
+    cin >> input;
 
 
-    /*
-    *   USER INTERFACE
-    * ------------------------------------------
-    */
-    cout << "\n                                                                                     ";
-    cout << "\n  Geben Sie den nächsten Zug ein. (Format: d7-a4)"
-            "\n  Zum Verlassen 'exit' Eingeben. ";
-
-
-
-    // close Game
-    if (userInputString == "exit")
+    // Checks exit command and sanity
+    if (input == "exit")
+    {
       return 0;
+    }
+    else if (regex_match(input, pattern))
+    {
+      // convert input to numerical index of board array.
+      fromx = input[0] - 97;
+      fromy = input[1] - 49;
+      tox = input[3] - 97;
+      toy = input[4] - 49;
 
-    // unexpected input
-    if (userInputString.length() != 5)
-      continue;
-    //
-    strcpy( userInputArray, cin.c_str());
+      // move
+      board[toy][tox] = board[fromy][fromx];
+      board[fromy][fromx] = fF;
 
-return 0;
+    }
+
   } //while true
 
 };
-
-
-
 
